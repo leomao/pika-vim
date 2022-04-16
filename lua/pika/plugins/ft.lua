f2496b3cffb53ec9460bb0ff66185dbc9717005a
Leo Mao <leomaoyw@gmail.com>
2022-04-16 22:16:50 +0800

    Add python syntax and pep8 indent.

--- lua/pika/plugins/ft.lua
+++ lua/pika/plugins/ft.lua
@@ -1,6 +1,8 @@
 local M = {}
 
 function M.setup(use)
+  use { 'vim-python/python-syntax', ft = 'python' }
+  use { 'Vimjas/vim-python-pep8-indent', ft = 'python' }
   use { 'rust-lang/rust.vim', ft = 'rust' }
   use { 'lervag/vimtex', ft = 'tex' }
 end
