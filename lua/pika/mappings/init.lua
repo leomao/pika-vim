1d7aaf5471da8f98abba62a36e3418cba9b82ca9
Leo Mao <leomaoyw@gmail.com>
2022-06-16 23:34:12 +0800

    Use fzf-lua and sk as fuzzy finder.

--- lua/pika/mappings/init.lua
+++ lua/pika/mappings/init.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+local map = vim.keymap.set
+
+local function simap(mode, lhs, rhs, opts)
+  if opts == nil then
+    opts = {}
+  end
+  opts.silent = true
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 M.default_options = {
   bufferline = true,
   fzf = true,
@@ -15,50 +25,47 @@ function M.setup(options)
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
 
-  local noremap = require('pika.mappings.util').noremap
-  local noresimap = require('pika.mappings.util').noresimap
-
   vim.opt.pastetoggle = '<F2>'
   -- Swap j <-> gj, k <-> gk. It is more intuitive when moving in long lines.
-  noremap('n', 'j', 'gj')
-  noremap('n', 'k', 'gk')
-  noremap('n', 'gj', 'j')
-  noremap('n', 'gk', 'k')
+  map('n', 'j', 'gj')
+  map('n', 'k', 'gk')
+  map('n', 'gj', 'j')
+  map('n', 'gk', 'k')
   -- Swap 0 <-> ^. 0 is easier to press while the original ^ is more useful.
-  noremap('n', '0', '^')
-  noremap('n', '^', '0')
-  noremap('v', '0', '^')
-  noremap('v', '^', '0')
+  map('n', '0', '^')
+  map('n', '^', '0')
+  map('v', '0', '^')
+  map('v', '^', '0')
   -- Make +/- be increasing/decreasing the number.
-  noremap('n', '+', '<C-a>')
-  noremap('n', '-', '<C-x>')
+  map('n', '+', '<C-a>')
+  map('n', '-', '<C-x>')
   -- Tmux uses <C-a>. Let's use <C-b> instead.
-  noremap('i', '<C-b>', '<C-a>')
+  map('i', '<C-b>', '<C-a>')
   -- Make window resizing easier.
-  noremap('n', '<C-Right>', '<C-w>2>')
-  noremap('n', '<C-Left>', '<C-w>2<')
-  noremap('n', '<C-Up>', '<C-w>+')
-  noremap('n', '<C-Down>', '<C-w>-')
+  map('n', '<C-Right>', '<C-w>2>')
+  map('n', '<C-Left>', '<C-w>2<')
+  map('n', '<C-Up>', '<C-w>+')
+  map('n', '<C-Down>', '<C-w>-')
 
-  noresimap('n', '<Leader>/', '<Cmd>nohl<CR>')
-  noresimap('n', '<Leader>w', '<Cmd>w!<CR>')
-  noresimap('n', '<Leader>q', '<Cmd>bdelete<CR>')
-  noresimap('n', '<Leader><Leader>', '<Cmd>b #<CR>')
+  simap('n', '<Leader>/', '<Cmd>nohl<CR>')
+  simap('n', '<Leader>w', '<Cmd>w!<CR>')
+  simap('n', '<Leader>q', '<Cmd>bdelete<CR>')
+  simap('n', '<Leader><Leader>', '<Cmd>b #<CR>')
 
   if opts.bufferline then
-    noresimap('n', 'L', '<Cmd>BufferLineCycleNext<CR>')
-    noresimap('n', 'H', '<Cmd>BufferLineCyclePrev<CR>')
-    noresimap('n', '<Leader>L', '<Cmd>BufferLineMoveNext<CR>')
-    noresimap('n', '<Leader>H', '<Cmd>BufferLineMovePrev<CR>')
+    simap('n', 'L', '<Cmd>BufferLineCycleNext<CR>')
+    simap('n', 'H', '<Cmd>BufferLineCyclePrev<CR>')
+    simap('n', '<Leader>L', '<Cmd>BufferLineMoveNext<CR>')
+    simap('n', '<Leader>H', '<Cmd>BufferLineMovePrev<CR>')
   else
-    noresimap('n', 'L', '<Cmd>bnext<CR>')
-    noresimap('n', 'H', '<Cmd>bprev<CR>')
+    simap('n', 'L', '<Cmd>bnext<CR>')
+    simap('n', 'H', '<Cmd>bprev<CR>')
   end
 
   if opts.fzf then
-    noresimap('n', '<Leader>ff', '<Cmd>Files<CR>')
-    noresimap('n', '<Leader>fb', '<Cmd>Buffers<CR>')
-    noresimap('n', '<Leader>fg', '<Cmd>Rg<CR>')
+    simap('n', '<Leader>ff', '<Cmd>FzfLua files<CR>')
+    simap('n', '<Leader>fb', '<Cmd>FzfLua buffers<CR>')
+    simap('n', '<Leader>fg', '<Cmd>FzfLua grep<CR>')
   end
 end
 
--- lua/pika/plugins/picker.lua
+++ lua/pika/plugins/picker.lua
@@ -1,13 +1,36 @@
 local M = {}
 
-function M.setup(use)
-  -- fuzzy search
-  use {
-    'junegunn/fzf.vim',
-    requires = {
-      {'junegunn/fzf'},
+local function fzf_config()
+  local actions = require('fzf-lua.actions')
+  local fzf = require('fzf-lua')
+  fzf.setup({
+    fzf_bin = 'sk',
+    actions = {
+      files = {
+        ["default"] = actions.file_edit,
+      },
     },
+  })
+  fzf.register_ui_select()
+end
+
+function M.setup(use)
+  local sk_plugin = { 'lotabout/skim' }
+  if not vim.fn.executable('sk') then
+    sk_plugin.run = './install'
+  end
+
+  use(sk_plugin)
+
+  use { 'ibhagwan/fzf-lua',
+    -- optional for icon support
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      -- 'junegunn/fzf',
+    },
+    config = fzf_config,
   }
+
 end
 
 return M
