622dd26ccd5459e7dbb85e093a357b8005a96b20
Leo Mao <leomaoyw@gmail.com>
2023-02-25 16:19:31 +0800

    Add flatbuffers syntax support.

--- lua/pika/plugins/ft.lua
+++ lua/pika/plugins/ft.lua
@@ -3,4 +3,5 @@ return {
   { "Vimjas/vim-python-pep8-indent", ft = "python" },
   { "rust-lang/rust.vim", ft = "rust" },
   { "lervag/vimtex", ft = "tex" },
+  { "dcharbon/vim-flatbuffers", ft = "fbs" },
 }
